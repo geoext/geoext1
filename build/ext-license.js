73959d86fcc3f7c7699fc4ae520645b52376a95a
fredj <frederic.junod@camptocamp.com>
2008-12-15 09:56:09 +0000

    fix ExtJS version and license URL

--- build/ext-license.js
+++ build/ext-license.js
@@ -1,8 +1,8 @@
 /*
- * Ext JS Library 2.1
+ * Ext JS Library 2.2
  * Copyright(c) 2006-2008, Ext JS, LLC.
  * licensing@extjs.com
  * 
- * http://extjs.com/license
+ * http://extjs.com/products/license.php
  */
 
