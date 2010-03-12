9d643b525bb51965dd377cf7643702e59aa7c490
Tim Schaub <tim.schaub@gmail.com>
2010-03-12 15:59:48 +0000

    Copyright update.

--- geoext/lib/GeoExt/widgets/LayerOpacitySlider.js
+++ geoext/lib/GeoExt/widgets/LayerOpacitySlider.js
@@ -1,4 +1,4 @@
-/* Copyright (C) 2008-2009 The Open Source Geospatial Foundation
+/* Copyright (c) 2008-2010 The Open Source Geospatial Foundation
  * Published under the BSD license.
  * See http://geoext.org/svn/geoext/core/trunk/license.txt for the full text
  * of the license.
