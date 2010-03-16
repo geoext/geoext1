388a0871670f4bdbd98b2f000e2831cd9d9e4116
Éric Lemoine <eric.lemoine@gmail.com>
2010-03-16 13:56:39 +0000

    make the MapPanel stateful, so map position, layer visibility, and layer opacity can be stored and restored through an Ext.state.Provider, r=bartvde (closes #94)

--- examples/mappanel-div.js
+++ examples/mappanel-div.js
@@ -15,6 +15,7 @@
 var mapPanel;
 
 Ext.onReady(function() {
+    Ext.state.Manager.setProvider(new Ext.state.CookieProvider());
     var map = new OpenLayers.Map();
     var layer = new OpenLayers.Layer.WMS(
         "Global Imagery",
@@ -26,11 +27,25 @@ Ext.onReady(function() {
     mapPanel = new GeoExt.MapPanel({
         title: "GeoExt MapPanel",
         renderTo: "mappanel",
+        stateId: "mappanel",
         height: 400,
         width: 600,
         map: map,
         center: new OpenLayers.LonLat(5, 45),
-        zoom: 4
+        zoom: 4,
+        // getState and applyState are overloaded so panel size
+        // can be stored and restored
+        getState: function() {
+            var state = GeoExt.MapPanel.prototype.getState.apply(this);
+            state.width = this.getSize().width;
+            state.height = this.getSize().height;
+            return state;
+        },
+        applyState: function(state) {
+            GeoExt.MapPanel.prototype.applyState.apply(this, arguments);
+            this.width = state.width;
+            this.height = state.height;
+        }
     });
 });
 
