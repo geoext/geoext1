5db5c6ef175e2c38fb79cdfbb5dbd261262cb9c5
Tim Schaub <tim.schaub@gmail.com>
2010-03-17 17:31:45 +0000

    Docs for vector legend example.

--- examples/vector-legend.js
+++ examples/vector-legend.js
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2008-2010 The Open Source Geospatial Foundation
+ * 
+ * Published under the BSD license.
+ * See http://svn.geoext.org/core/trunk/geoext/license.txt for the full text
+ * of the license.
+ */
+
+/** api: example[vector-legend]
+ *  Vector Legend
+ *  -------------------------
+ *  Render a legend for a vector layer.
+ */
+
 var mapPanel, legendPanel;
 
 Ext.onReady(function() {
