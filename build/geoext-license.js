17d908ec04cb8d6793896ce960f50745b8742311
ahocevar <andreas.hocevar@gmail.com>
2012-02-12 01:10:12 +0100

    Updating copyright year

--- build/geoext-license.js
+++ build/geoext-license.js
@@ -1,6 +1,6 @@
 /*
 
-Copyright (c) 2008-2011, The Open Source Geospatial Foundation
+Copyright (c) 2008-2012, The Open Source Geospatial Foundation
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
